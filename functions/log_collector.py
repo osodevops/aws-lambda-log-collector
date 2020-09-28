f35568cd93871c1afbcc9624a6ed7a8af050f036
tanelikantomaa <taneli.kantomaa@gmail.com>
2020-09-28 16:53:49 +0300

    remove other python function

--- functions/log_collector.py
+++ functions/log_collector.py
@@ -7,6 +7,7 @@ import boto3, json, time, logging, os
 from datetime import date, datetime, timedelta
 from botocore.exceptions import ClientError
 
+s3_bucket_name = os.environ['S3_BUCKET_NAME']
 # log_group_name = '/aws/eks/oso-ops/cluster'
 # s3_bucket_name = 'oso-ops-cw'
 # aws_region = 'eu-west-2'
@@ -16,6 +17,30 @@ end_number_of_days= 0
 timestring = datetime.fromtimestamp(time.time()).strftime('%Y%m%d-%Hh%Mm%Ss')
 ts=(int((datetime.today() - timedelta(hours=0.1 * start_number_of_days)).timestamp())) * 1000
 te=(int((datetime.today() - timedelta(hours=24 * end_number_of_days)).timestamp())) * 1000
+
+# logging init
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event,context):
+    data = json.dumps(event)
+    logger.info('Starting log collection based on this event:')
+    logger.info(data)
+
+    # grab parameters from our event
+    resource_id = event['resources'][0]
+    aws_region = event['region']
+    log_group_list = []
+
+    # init boto3
+    boto3.session.Session(profile_name="default")
+    client = boto3.client('logs', region_name=aws_region)
+    s3 = boto3.resource('s3')
+
+    response = client.describe_log_groups()
+    for i in response['logGroups']:
+        log_collector(i['logGroupName'], aws_region, s3_bucket_name)
+        time.sleep(15)
 
 
 def log_collector(logGroupName, awsRegion, s3BucketName):
