a1d15c90a7bf64c770a64145bcc705d6c000950c
Sion Smith <sion.smith@gmail.com>
2020-09-30 12:32:26 +0100

    Corrected S3 permissions for lambda :)

--- aws_lambda_iam_policy_document.tf
+++ aws_lambda_iam_policy_document.tf
@@ -1,3 +1,7 @@
+data "aws_s3_bucket" "compressed_logs_bucket" {
+  bucket = var.s3_bucket_name
+}
+
 data "aws_iam_policy_document" "lambda_config_trust" {
   statement {
     effect = "Allow"
@@ -13,8 +17,14 @@ data "aws_iam_policy_document" "lambda_config_trust" {
 data "aws_iam_policy_document" "lambda_config_policy" {
   statement {
     effect = "Allow"
-    actions   = ["s3:*"]
-    resources = ["*"]
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${data.aws_s3_bucket.compressed_logs_bucket.arn}/*",
+    ]
+
   }
 
   statement {
